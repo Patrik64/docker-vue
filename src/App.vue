bc6d80b1433a653ed9bbac64c55777f7eab53f58
patrik simic <patrik.simic@gmail.com>
2020-05-13 11:15:19 +0200

    added tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
-  <div id="app">
+  <div id="app" class="bg-grey-900 w-full h-full">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/tailwindtest">Tailwind Test</router-link>
     </div>
     <router-view/>
   </div>
---
+++ src/views/TailwindTest.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home flex justify-center flex-col items-center">
+    <h1 class="text-5xl">TailwindTest</h1>
+    <div class="flex flex-col items-center bg-orange-400 w-1/3 p-10 rounded-lg shadow-2xl">
+        <img class="h-64 w-64 rounded-full"
+            src="https://source.unsplash.com/random"
+            alt=""
+        />
+        <h5 class="text-2xl font-semibold m-5">Some text about image</h5>
+    </div>
+  </div>
+</template>
